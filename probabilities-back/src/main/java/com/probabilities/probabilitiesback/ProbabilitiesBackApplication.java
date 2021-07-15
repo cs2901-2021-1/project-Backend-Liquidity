0c6846006dcde9e984abe976f578bfa9764caf78
guille2400 <guillermo.sanchez@utec.edu.pe>
2021-07-14 23:30:28 -0500

    Step by step

--- probabilities-back/src/main/java/com/probabilities/probabilitiesback/ProbabilitiesBackApplication.java
+++ probabilities-back/src/main/java/com/probabilities/probabilitiesback/ProbabilitiesBackApplication.java
@@ -2,8 +2,9 @@ package com.probabilities.probabilitiesback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class ProbabilitiesBackApplication {
 
 	public static void main(String[] args) {
